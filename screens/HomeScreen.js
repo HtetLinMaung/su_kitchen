876a96630906bf22346889befc3c7b02e9130c2f
Htet Lin Maung <jsthtet96@gmail.com>
2020-07-19 17:15:16 +0630

    bind api

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import Typography from "../components/Typography";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Typography>Home Screen</Typography>
+    </View>
+  );
+};
+
+export default HomeScreen;
